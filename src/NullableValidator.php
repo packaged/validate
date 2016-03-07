34beb7ea309a7394d661f935371d8761128d5cdf
richard.gooding <richard.gooding@justdevelop.it>
2016-03-07 13:48:47 +0000

    NullableValidator can now optionally treat empty strings as null

--- src/NullableValidator.php
+++ src/NullableValidator.php
@@ -4,19 +4,31 @@ namespace Packaged\Validate;
 class NullableValidator extends AbstractValidator
 {
   private $_validator;
+  private $_allowEmptyString = false;
 
-  public function __construct(IValidator $validator)
+  /**
+   * @param IValidator $validator
+   * @param bool       $allowEmptyString If true then treat empty strings as nulls
+   */
+  public function __construct(IValidator $validator, $allowEmptyString = false)
   {
     $this->_validator = $validator;
+    $this->_allowEmptyString = $allowEmptyString;
   }
 
   public function validate($value)
   {
-    return ($value === null) || $this->_validator->validate($value);
+    return ($value === null)
+    || ($this->_allowEmptyString && ($value === ''))
+    || $this->_validator->validate($value);
   }
 
   public function tidy($value)
   {
+    if($this->_allowEmptyString && ($value === ''))
+    {
+      $value = null;
+    }
     if($value !== null)
     {
       $value = $this->_validator->tidy($value);
