550c07010c85ca291ff2a37b8331ee573bc41dd7
Tom Kay <oridan82@gmail.com>
2020-08-21 14:09:47 +0100

    fix error message case

--- src/Validators/EmailValidator.php
+++ src/Validators/EmailValidator.php
@@ -3,7 +3,7 @@ namespace Packaged\Validate\Validators;
 
 class EmailValidator extends RegexValidator
 {
-  public function __construct($message = 'Invalid email address')
+  public function __construct($message = 'invalid email address')
   {
     parent::__construct(
       '/^[_a-zA-Z0-9+\-]+(\.[_a-zA-Z0-9+\-]+)*@[a-zA-Z0-9\-]+(\.[a-zA-Z0-9\-]+)*(\.[a-zA-Z]{2,})$/',
