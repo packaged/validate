da5f3a7d53ee11ffab4a31ee11be8000172d5fe7
Tom Kay <oridan82@gmail.com>
2020-08-24 11:12:01 +0100

    required validator

---
+++ src/Validators/RequiredValidator.php
@@ -0,0 +1,27 @@
+<?php
+namespace Packaged\Validate\Validators;
+
+use Generator;
+use Packaged\Validate\AbstractSerializableValidator;
+use Packaged\Validate\SerializableValidator;
+
+class RequiredValidator extends AbstractSerializableValidator
+{
+  public static function deserialize($configuration): SerializableValidator
+  {
+    return new static();
+  }
+
+  public function serialize(): array
+  {
+    return [];
+  }
+
+  protected function _doValidate($value): Generator
+  {
+    if($value === null || $value === '')
+    {
+      yield $this->_makeError('required');
+    }
+  }
+}
---
+++ tests/RequiredValidatorTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Packaged\Validate\Tests;
+
+use Packaged\Validate\Validators\RequiredValidator;
+use PHPUnit\Framework\TestCase;
+
+class RequiredValidatorTest extends TestCase
+{
+  public function testRequired()
+  {
+    $validator = new RequiredValidator();
+    $this->assertFalse($validator->isValid(null));
+    $this->assertFalse($validator->isValid(''));
+    $this->assertTrue($validator->isValid('test'));
+    $this->assertTrue($validator->isValid(0));
+    $this->assertTrue($validator->isValid(false));
+    $this->assertTrue($validator->isValid(true));
+  }
+}
